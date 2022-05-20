5a99786dc0d2d2f9e883d1f93baffb742c7ecdfc
sprinter10111 <59802922+sprinter10111@users.noreply.github.com>
2022-05-20 13:17:47 +0200

    na domme fout

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const fs = require("fs");
+
+app.set("port", 3000);
+app.set("view engine", "ejs");
 
 app.get('/',(req,res)=>{
-    app.render("iets");
+    res.render("iets");
     
-})
-    
-    
+});
+/*app.set('port',(process.env.PORT||5000));
+app.listen(app.get('port'));*/
 
-
-app.set('port',(process.env.PORT||5000));
-app.listen(app.get('port'));
+app.listen(app.get("port"), () => {
+    console.log(`Web application started at http://localhost:${app.get("port")}`);
+  });
+  
