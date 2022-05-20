c03aa63bf1d9c083a52c1ad25088c499b05812ad
sprinter10111 <59802922+sprinter10111@users.noreply.github.com>
2022-05-20 12:56:01 +0200

    change

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    var fs = require('fs');
-
-    function readModuleFile(path, callback) {
-    try {
-        var filename = require.resolve(path);
-        fs.readFile(filename, 'utf8', callback);
-    } catch (e) {
-        callback(e);
-    }
-}
-
-    readModuleFile('./view/index.txt', function (err, words) {
-        res.type('text/html');
-        res.send(words);
+    app.render("iets");
+    
 })
     
     
-});
+
 
 app.set('port',(process.env.PORT||5000));
 app.listen(app.get('port'));
